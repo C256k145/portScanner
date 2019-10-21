1c98a9a3e8d82f42f53a029d249f45baa70e9d04
c256k145 <noremackientz@gmail.com>
2019-10-21 12:47:04 -0500

    changed scanning mode from null to tcp connect, since mac and windows never sent any response to a null packet

--- portScanner.py
+++ portScanner.py
@@ -9,26 +9,28 @@ closedPorts = []
 filteredPorts = []
 
 progressIncrement = 0
-portTotal = 1023
+portTotal = 1000
 # Sends a null packet with no flags to a port. An open port will send no response, a closed port would send a 
 # response packet with a RST(reset) flag, and an ICMP filtered port will send an ICMP error response.
 for port in range(1,portTotal):
+    # This is just for the progress indicator
     progress = round((port/portTotal)*100)
     if(port % 20 == 0):
         progressIncrement += 1
     sys.stdout.write(f'\r{str(progress)}% [{"="*(progressIncrement)}{"_"*(round(portTotal/20)-progressIncrement)}]')
     sys.stdout.flush()
-    packet = sr1(IP(dst=target)/TCP(dport=port,flags=""),timeout=10,verbose=False)
+    # This is where the actual packet sending starts
+    packet = sr1(IP(dst=target)/TCP(dport=port,flags="S"),timeout=1,verbose=False)
     if packet is None:
-        openPorts.append(port)
+        closedPorts.append(port)
     elif(packet.haslayer(TCP)):
-        if(packet.getlayer(TCP).flags == 0x14):
+        if(packet.getlayer(TCP).flags == 0x12):
+            openPorts.append(port)
+        elif(packet.getlayer(TCP).flags == 0x14):
+            # if(int(packet.getlayer(ICMP).type) == 3 and int(packet.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+                # filteredPorts.append(port)
             closedPorts.append(port)
-        elif(packet.haslayer(ICMP)):
-            if(int(packet.getlayer(ICMP).type) == 3 and int(packet.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-                filteredPorts.append(port)
 
-print()
-print("Open ports: " + str(openPorts))
-print("Filtered ports: " + str(filteredPorts))
+print("\nOpen ports: " + str(openPorts))
+# print("Filtered ports: " + str(filteredPorts))
 
